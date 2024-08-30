db9a5e241ac7cd15cbf40098dcf810c55e5bb0de
chan2242 <chan2242@umn.edu>
2024-08-29 21:00:52 -0500

    added spring boot starter files

---
+++ server/src/main/java/com/accountability_notification_system/accountability_notification_system/AccountabilityNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.accountability_notification_system.accountability_notification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilityNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountabilityNotificationSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/accountability_notification_system/accountability_notification_system/AccountabilityNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountability_notification_system.accountability_notification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountabilityNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
