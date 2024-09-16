7e01be286cdd8587a9672cce94caf8df229f06c2
Elsdon Chang <chan2242@umn.edu>
2024-09-15 21:14:12 -0500

    feat: auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,36 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null);
 
-  return (
-    <>
-    </>
-  )
+  useEffect(() => {
+    fetchUser();
+    if (user) {
+      console.log("logged in");
+      window.location.href = "http://localhost:5173/dashboard";
+    }
+  }, []);
+
+  const fetchUser = async () => {
+    console.log("Fetching user");
+    try {
+      let response = await fetch(userEndpoint, {
+        method: "GET",
+        credentials: "include",
+      });
+      response = await response.json();
+      setUser(response["name"]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    window.location.href = "http://localhost:8080/oauth2/authorization/google";
+  };
+  
+  return <></>;
 }
 
-export default App
+export default App;
