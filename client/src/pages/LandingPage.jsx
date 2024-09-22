68ccc938636949a968bd1ead03e3d863db50ab2b
Elsdon Chang <chan2242@umn.edu>
2024-09-21 23:59:38 -0500

    added content and redirection logic to LandingPage

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function LandingPage() {
+function LandingPage({ handleSignIn, user }) {
+  if (user != null) {
+    window.location = "/dashboard";
+  }
   return (
-    <div>LandingPage</div>
-  )
+    <div>
+      <h1 className={"headline"}>Reclaim Your Time</h1>
+      <p className={"subheadline"}>
+        Stay Accountable and Focus on What Matters
+      </p>
+      <button onClick={() => handleSignIn()} className={"cta"}>
+        Break Your Addiction Now
+      </button>
+      <p className={"reassurance"}>
+        Your details will not be shared with anyone.
+      </p>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
