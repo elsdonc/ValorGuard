52929c14ee5deb0d162da9326dc314c7972336d1
Elsdon Chang <chan2242@umn.edu>
2024-09-29 00:38:33 -0500

    added custom exception for ValorantMatchHistoryScraperService

---
+++ server/src/main/java/com/accountability_notification_system/accountability_notification_system/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.accountability_notification_system.accountability_notification_system.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
