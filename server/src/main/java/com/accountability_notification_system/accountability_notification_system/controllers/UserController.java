b694662efbbed5e022143ed4a5700f49cee148e4
Elsdon Chang <chan2242@umn.edu>
2024-09-29 00:45:10 -0500

    refactor: put logic in service files

--- server/src/main/java/com/accountability_notification_system/accountability_notification_system/controllers/UserController.java
+++ server/src/main/java/com/accountability_notification_system/accountability_notification_system/controllers/UserController.java
@@ -14,53 +14,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.accountability_notification_system.accountability_notification_system.model.User;
-import com.accountability_notification_system.accountability_notification_system.repositories.UserRepository;
+import com.accountability_notification_system.accountability_notification_system.exceptions.UserNotFoundException;
+import com.accountability_notification_system.accountability_notification_system.services.UserDetailService;
+import com.accountability_notification_system.accountability_notification_system.services.UserUpdateService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserDetailService userDetailService;
+
+    @Autowired
+    private UserUpdateService userUpdateService;
 
     @GetMapping("/user")
     public Map<String, String> getUser(@AuthenticationPrincipal OAuth2User oAuth2User) {
-        Map<String, String> map = new HashMap<>();
-        if (oAuth2User != null) {
-            String name = oAuth2User.getAttribute("name");
-            String email = oAuth2User.getAttribute("email");
-            map.put("name", name);
-            map.put("email", email);
-        } else {
-            map.put("error", "User not authenticated");
-        }
-        return map;
+        return userDetailService.getUserDetails(oAuth2User);
     }
 
     @PostMapping("/user/saveData")
     @ResponseStatus(HttpStatus.CREATED)
     public Map<String, String> saveUserData(@AuthenticationPrincipal OAuth2User oAuth2User, @RequestBody Map<String, String> json) {
-        User user = userRepository.findByEmail(oAuth2User.getAttribute("email"));
-        Map<String, String> map = new HashMap<>();
-        if (user != null) {
-            if (user.getValUser() != json.get("valUser")) {
-                user.setValUser(json.get("valUser"));
-            }
-            if (user.getValTag() != json.get("valTag")) {
-                user.setValTag(json.get("valTag"));
-            }
-            if (user.getNumber() != json.get("number")) {
-                user.setNumber(json.get("number"));
-            }
-            if (user.getAccPartnerNumber() != json.get("accPartnerNumber")) {
-                user.setAccPartnerNumber(json.get("accPartnerNumber"));
-            }
-            // TODO: set match count using scraping service
-            map.put("status", "success");
-        } else {
-            map.put("status", "error");
+        Map<String, String> response = new HashMap<>();
+        try {
+            userUpdateService.saveUserData(oAuth2User, json);
+            response.put("status", "success");
+        } catch (UserNotFoundException e) {
+            response.put("status", "error");
+            response.put("message", e.getMessage());
+        } catch (Exception e) {
+            response.put("status", "error");
+            response.put("message", "An error occurred while saving user data");
         }
-        return map;
+        return response;
     }
 }
