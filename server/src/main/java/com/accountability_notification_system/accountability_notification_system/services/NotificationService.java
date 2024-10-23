9ea0b28cb4f096f6d621ad6424274b982332de09
Elsdon Chang <chan2242@umn.edu>
2024-10-23 09:50:31 -0500

    changed text notification to include user's name

--- server/src/main/java/com/accountability_notification_system/accountability_notification_system/services/NotificationService.java
+++ server/src/main/java/com/accountability_notification_system/accountability_notification_system/services/NotificationService.java
@@ -1,6 +1,11 @@
 package com.accountability_notification_system.accountability_notification_system.services;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.twilio.Twilio;
@@ -9,14 +14,18 @@ import com.twilio.rest.api.v2010.account.Message;
 @Service
 public class NotificationService {
 
-    public void notify(@Value("${twilio.accountSid}") String accountSid, @Value("${twilio.authToken}") String authToken,
+    @Autowired
+    private UserDetailService userDetailService;
+
+    public void notify(@AuthenticationPrincipal OAuth2User oAuth2User, @Value("${twilio.accountSid}") String accountSid, @Value("${twilio.authToken}") String authToken,
             @Value("${twilio.phoneNumber}") String phoneNumber) {
 
+        Map<String, String> userDetails = userDetailService.getUserDetails(oAuth2User);
         Twilio.init(accountSid, authToken);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber(phoneNumber),
                 new com.twilio.type.PhoneNumber(phoneNumber),
-                "Hello from ValorGuard")
+                String.format("%s played a game.", userDetails.get("name")))
                 .create();
         System.out.println(message.getBody());
     }
